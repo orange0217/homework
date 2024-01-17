fd3837b03682628596384379c07e91c6818bc508
aaa <abc@qq.com>
2024-01-17 16:38:04 +0800

    aa

---
+++ cvm-only/variables copy.tf
@@ -0,0 +1,38 @@
+variable "secret_id" {
+  default = ""
+}
+
+variable "secret_key" {
+  default = ""
+}
+
+variable "region" {
+  default = "ap-hongkong"
+}
+variable "availability_zone" {
+  default = "ap-hongkong-3"
+}
+variable "password" {
+  default = "password123"
+}
+
+variable "domain" {
+  default = "starmooc.net"
+}
+
+variable "apiID" {
+  description = "dnspod apiID"
+  default = "465516"
+}
+
+variable "apiToken" {
+  description = "dnspod apiToken"
+  default = ""
+}
+variable "cpu" {
+  default = "2"
+}
+
+variable "memory" {
+  default = "4"
+}
